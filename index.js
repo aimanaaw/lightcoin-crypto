b3aa9284deae1205ff39f04bc7f2c5aadc267390
Aiman Azmie <aimanaaw@gmail.com>
2019-09-28 18:18:24 +0000

    The account transaction exercise

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-let balance = 500.00;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    let x = this.transactions.reduce((acc, curr) => acc + curr.value, 0);
+    return x;
 
-class Withdrawal {
-
-  constructor(amount) {
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    //Keep track of the time of the transaction
+    if(this.isAllowed === true) {
+      this.time = new Date();
+      //Add the transaction to the account
+      this.account.addTransaction(this);
+      }
+  }
+}
+
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+    }
+    get isAllowed() {
+      if (this.value < 0) {
+        return false;
+      }
+    }
   }
 
+class Deposit extends Transaction {
+  get value() {
+    return this.amount;
+  }
+  get isAllowed() {
+    if (this.value > 0) {
+      return true;
+
+    }
+  }
 }
 
 
@@ -17,13 +55,19 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
-
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log("\n\nCheck Balance!!!!\n", myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('\n\nTransaction 2:', t2);
+console.log("\n\nCheck Balance!!!!\n", myAccount.balance);
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('\n\nTransaction 3:', t3);
+
+console.log("\n\nCheck Balance!!!!", myAccount.balance);
